a02ba4823a5b8fe52e2dddb39b95488dc5fd1cc5
fnya <fnyamail@gmail.com>
2023-10-25 19:06:55 +0900

    Implemented.

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Remove Empty Folders
 
 This is an Obsidian plugin that removes empty folders.
+
+## Caution !!
+
+**Be sure to back up Obisidan data before running this plugin !!**
+
+**In the worst case scenario, data may be lost.**
+
+## How to use
+
+Select the notes you want to merge in the file explorer.
+
+Select the folder(s) in the File Explorer from which you want to remove the empty folder(s).
+
+Right click on the selection and select `Remove empty folders`.
+
+<img src="resources/image01.png" width="320">
+
+In the image example, `subfulder1-2` and `subfulder1-3` will be removed.
+
+## Limitations
+
+This plugin removes empty folders at the point of execution. Therefore, empty folders as a result of running the plugin will not be removed.
+
+You may need to run the plugin several times to remove all empty folders.
+
+## Troubleshootings
+
+In the unlikely event that an unexpected folder is deleted, please check inside the `ObsidianValtPath/.trash` folder.
+
+The plugin moves the empty folder to the obsidian `.trash` folder rather than physically removing it, so the contents can be checked and recovered as shown in the following image.
+
+<img src="resources/image02.png" width="200">
--- main.ts
+++ main.ts
@@ -1,20 +1,17 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { App, Plugin, TFolder } from "obsidian";
 
-export default class MyPlugin extends Plugin {
+export default class RemoveEmptyFoldersPlugin extends Plugin {
 	async onload() {
 		this.registerEvent(
 			this.app.workspace.on("files-menu", (menu, files) => {
 				menu.addItem((item) => {
-					item.setTitle("Remove empty folders").onClick(async () => {});
+					item.setTitle("Remove empty folders").onClick(async () => {
+						files.forEach((file) => {
+							if (file instanceof TFolder) {
+								removeEmptyFolders(this.app, file as TFolder);
+							}
+						});
+					});
 				});
 			})
 		);
@@ -22,11 +19,25 @@ export default class MyPlugin extends Plugin {
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
 				menu.addItem((item) => {
-					item.setTitle("Remove empty folders").onClick(async () => {});
+					item.setTitle("Remove empty folders").onClick(async () => {
+						if (file instanceof TFolder) {
+							removeEmptyFolders(this.app, file as TFolder);
+						}
+					});
 				});
 			})
 		);
 	}
-
-	onunload() {}
 }
+
+const removeEmptyFolders = (app: App, folder: TFolder) => {
+	folder.children.forEach((child) => {
+		if (child instanceof TFolder) {
+			removeEmptyFolders(app, child);
+		}
+	});
+
+	if (folder.children.length === 0) {
+		app.vault.adapter.trashLocal(folder.path);
+	}
+};
